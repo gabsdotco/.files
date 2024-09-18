ca4042b27fcae7dcae5cfe6d6b9db9f36fa66f43
gabsdotco <biewxw@gmail.com>
2024-09-18 02:09:10 -0300

    fix(neovim): remove packer installation since now it was replaced by lazy

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -15,10 +15,6 @@ sudo apt install fd-find
 echo "Installing Bob"
 cargo install bob-nvim
 
-# Installing Packer
-echo "Installing Packer"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 # Clone the Neovim repository to .config/nvim
 echo "Cloning the custom Neovim configuration"
 git clone https://github.com/gabsdotco/init.lua.git $HOME/.config/nvim
