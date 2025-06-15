239ab0603798267fd2df862a95f8473fa55190ca
gabs <gabriel.oliveira@fingermarkglobal.com>
2025-06-15 16:25:51 -0300

    feat(scripts): add rust installation script

--- scripts/rust.sh
+++ scripts/rust.sh
@@ -3,9 +3,19 @@
 echo "
 #####################
 # Rust Installation #
-#####################
-"
+#####################"
 
-# # Installing Rust
-echo "Installing Rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# Installing Rust
+echo ""
+echo "> Installing Rust"
+echo ""
+
+if [ -e "~/.cargo/bin/rustc" ]; then
+    echo "[!] Rust is not installed. Installing now..."
+    echo ""
+
+    # Install Rust using rustup
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+else
+    echo "[!] Rust is already installed."
+fi
